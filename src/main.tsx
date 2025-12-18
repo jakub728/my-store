dae7e30fa82da99f8870b3e9217d47f41142810e
Jakub <sulisz.jakub@gmail.com>
2025-12-18 16:32:15 +0100

    HashRouter update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+//ze wzgledu na github pages
 import "./index.css";
 import App from "./App.tsx";
 import ProductsProvider from "./context/ProductsContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ProductsProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ProductsProvider>
 );
