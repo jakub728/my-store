bd6f7aaca9ae2a5297d527b51e203e7e270af84b
Jakub <sulisz.jakub@gmail.com>
2025-12-18 16:33:20 +0100

    HashRouter update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
 import Products from "./components/Products/Products";
@@ -10,18 +10,16 @@ import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/basket" element={<Basket />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/basket" element={<Basket />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
 
-        <ToastContainer position="top-right" autoClose={2000} />
-      </HashRouter>
+      <ToastContainer position="top-right" autoClose={2000} />
     </>
   );
 }
